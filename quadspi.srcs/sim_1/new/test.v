aa56bc7156b9c977d2792ecc364be8947a516eb7
bakul14 <m_bakulevskiy@baumanracing.ru>
2024-09-24 18:55:34 +0300

    (-) deleted copy-pasted master, slave modules from test source

--- quadspi.srcs/sim_1/new/test.v
+++ quadspi.srcs/sim_1/new/test.v
@@ -1,141 +1,3 @@
-module Master (
-	clk,
-	reset,
-	start,
-	slaveSelect,
-	masterDataToSend,
-	masterDataReceived,
-	sclk,
-	CS,
-	MOSI_0,
-	MOSI_1,
-	MOSI_2,
-	MOSI_3,
-	MISO_0,
-	MISO_1,
-	MISO_2,
-	MISO_3
-);
-	input wire clk;
-	input wire reset;
-	input wire start;
-	input wire slaveSelect;
-	input wire [7:0] masterDataToSend;
-	output reg [7:0] masterDataReceived;
-	output wire sclk;
-	output reg CS;
-	output reg MOSI_0;
-	output reg MOSI_1;
-	output reg MOSI_2;
-	output reg MOSI_3;
-	input wire MISO_0;
-	input wire MISO_1;
-	input wire MISO_2;
-	input wire MISO_3;
-	reg [7:0] Register;
-	assign sclk = clk;
-	reg transmit = 0;
-	integer maxCount = 0;
-	always @(posedge start or posedge reset) begin
-		if (!transmit && start) begin
-			Register <= masterDataToSend;
-			masterDataReceived <= 8'bxxxxxxxx;
-			maxCount <= 0;
-			transmit <= 1;
-			if (slaveSelect == 0)
-				CS <= 1'b0;
-			else
-				CS <= 1'b1;
-		end
-		if (reset)
-			Register <= 0;
-	end
-	always @(posedge clk)
-		if (transmit) begin
-			Register <= Register << 4;
-			MOSI_0 <= Register[4];
-			MOSI_1 <= Register[5];
-			MOSI_2 <= Register[6];
-			MOSI_3 <= Register[7];
-		end
-	always @(negedge clk) begin
-		if (maxCount >= 5) begin
-			transmit <= 0;
-			CS <= 1'b1;
-		end
-		if (transmit) begin
-			Register[0] <= MISO_0;
-			Register[1] <= MISO_1;
-			Register[2] <= MISO_2;
-			Register[3] <= MISO_3;
-			masterDataReceived <= {masterDataReceived[3:0], MISO_3, MISO_2, MISO_1, MISO_0};
-			maxCount <= maxCount + 1;
-		end
-	end
-endmodule
-module Slave (
-	reset,
-	slaveDataToSend,
-	slaveDataReceived,
-	sclk,
-	CS,
-	MOSI_0,
-	MOSI_1,
-	MOSI_2,
-	MOSI_3,
-	MISO_0,
-	MISO_1,
-	MISO_2,
-	MISO_3
-);
-	input wire reset;
-	input wire [7:0] slaveDataToSend;
-	output reg [7:0] slaveDataReceived;
-	input wire sclk;
-	input wire CS;
-	input wire MOSI_0;
-	input wire MOSI_1;
-	input wire MOSI_2;
-	input wire MOSI_3;
-	output reg MISO_0;
-	output reg MISO_1;
-	output reg MISO_2;
-	output reg MISO_3;
-	reg [7:0] Register;
-	reg flag = 0;
-	always @(negedge CS or posedge reset) begin
-		if (CS == 0) begin
-			Register <= slaveDataToSend;
-			slaveDataReceived <= 8'bxxxxxxxx;
-			flag <= 1;
-		end
-		if (reset)
-			Register <= 0;
-	end
-	always @(posedge CS) begin
-		flag <= 0;
-		MISO_0 <= 1'bz;
-		MISO_1 <= 1'bz;
-		MISO_2 <= 1'bz;
-		MISO_3 <= 1'bz;
-	end
-	always @(posedge sclk)
-		if (flag) begin
-			Register <= Register << 4;
-			MISO_0 <= Register[4];
-			MISO_1 <= Register[5];
-			MISO_2 <= Register[6];
-			MISO_3 <= Register[7];
-		end
-	always @(negedge sclk)
-		if (flag) begin
-			Register[0] = MOSI_0;
-			Register[1] = MOSI_1;
-			Register[2] = MOSI_2;
-			Register[3] = MOSI_3;
-			slaveDataReceived <= {slaveDataReceived[3:0], MOSI_3, MOSI_2, MOSI_1, MOSI_0};
-		end
-endmodule
 module test;
 	reg clk = 0;
 	reg reset;
@@ -159,7 +21,7 @@ module test;
 	always @(*) SCLK = sv2v_tmp_m1_sclk;
 	wire [1:1] sv2v_tmp_m1_CS;
 	always @(*) CS = sv2v_tmp_m1_CS;
-	Master m1(
+	Master master(
 		.clk(clk),
 		.reset(reset),
 		.start(start),
@@ -177,7 +39,7 @@ module test;
 		.MISO_2(MISO_2),
 		.MISO_3(MISO_3)
 	);
-	Slave flash(
+	Slave slave(
 		.reset(reset),
 		.slaveDataToSend(slaveDataToSend),
 		.slaveDataReceived(slaveDataReceived),
